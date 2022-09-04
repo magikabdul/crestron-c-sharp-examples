f18626d0e4c787501e258b7001eeef801e852044
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-09-04 17:36:09 +0200

    [basic XPanel] - added ps script file

---
+++ basic-XPanel/basic-xpanel.ps1
@@ -0,0 +1,6 @@
+﻿$file = "D:\crestron\c-sharp-crestron-examples\basic-XPanel\basic-XPanel\bin\Debug\basic_XPanel.cpz"
+$ip = "192.168.1.85"
+$slotNumber = 1
+$username = "admin"
+$password = "Cr@str0N"
+Send-CrestronProgram -Device $ip -LocalFile $file -Password $password -ProgramSlot $slotNumber -Secure -Username $username
